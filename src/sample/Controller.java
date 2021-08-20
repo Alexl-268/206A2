9bde3b05dc589a2ea54447af64a1e8a0b00657c2
Alexl-268 <zlia715@aucklanduni.ac.nz>
2021-08-20 20:09:12 +1200

    finished formatting

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,43 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
+
+import java.awt.*;
+import java.io.IOException;
+
 public class Controller {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    private AnchorPane scenePane;
+
+    public void switchSceneMain(javafx.event.ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchSceneNew(javafx.event.ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("NewGame.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void quitApp(javafx.event.ActionEvent event) throws IOException {
+        stage = (Stage) scenePane.getScene().getWindow();
+        stage.close();
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,11 +13,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Spelling Game");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
